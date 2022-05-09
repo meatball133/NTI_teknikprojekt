066b903219e6891e6179b9528e1eb0d4b45e99b0
meatball <meatballsofmeatballs@gmail.com>
2022-05-09 14:37:17 +0200

    Update Teknik_projekt.py

--- Teknik_projekt.py
+++ Teknik_projekt.py
@@ -4,64 +4,67 @@ import requests
 import matplotlib.pyplot as plt
 import pandas as pd
 
-#class weather_led_strip(self):
-#def CheckToApi(self):
-datah = ""
-"""
-Settings:
-"""
-postHistoricalURL = "https://api.tomorrow.io/v4/timelines"
+class weather_led_strip:
+    def checkToApi():
+        datah = ""
+        """
+        Settings:
+        """
+        postHistoricalURL = "https://api.tomorrow.io/v4/timelines"
 
-# get your key from app.tomorrow.io/development/keys
+        # get your key from app.tomorrow.io/development/keys
 
-apikey = "Insert api key"
+        apikey = "Insert api key"
 
-location = ["Location (cordinates)"]
+        location = [""]
 
-fields = ["temperature",
-"precipitationType",
-"cloudCover",
-"precipitationIntensity"
-]
+        fields = ["temperature",
+        "precipitationType",
+        "cloudCover",
+        "precipitationIntensity"
+        ]
 
-units = "metric"
+        units = "metric"
 
-timesteps = ["current"]
+        timesteps = ["current"]
 
-now = dt.datetime.now(pytz.UTC)
+        now = dt.datetime.now(pytz.UTC)
 
-timezone = "Europe/Oslo"
-"""
-Gathering the data from the api.
-"""
-body = {"location": location, "fields": fields, "units": units, "timesteps": timesteps, "timezone":timezone}
-response = requests.post(f'{postHistoricalURL}?apikey={apikey}', json=body)
-data = response.json()
-data = data["data"]["timelines"]
-"""
-Converting the list that is recived to useable data
-"""
-datah = str(data)
-datah = datah.replace("]","")
-datah = datah.replace("[","")
-datah = datah.replace("}","")
-datah = datah.replace("{","")
-datah = datah.replace(":","")
-datah = datah.replace("'","")
-datah = datah.replace(",","")
-datah = list(datah.split(" "))
-cloud = float(datah[-7])# Say how much cloud there is, it gives a value from 0 to 100. Which is a value showing procentage.
-amountOfRain = float(datah[-5])# Says amount of precipitation in mm/hr, it can look like 2,45 and 0,15. It is a value above 0
-weather = int(datah[-3])
-""" Gives a value based on what type of precipitation, 
-    0 = no precipitation
-    1 = rain
-    2 = snow
-    3 = freezing rain
-    4 = ice pellet
-"""
-temprature = float(datah[-1])#Gives a value in celsius ít can look like -5 and 12,5
-print(datah)
+        timezone = "Europe/Oslo"
+        """
+        Gathering the data from the api.
+        """
+        body = {"location": location, "fields": fields, "units": units, "timesteps": timesteps, "timezone":timezone}
+        response = requests.post(f'{postHistoricalURL}?apikey={apikey}', json=body)
+        data = response.json()
+        data = data["data"]["timelines"]
+        """
+        Converting the list that is recived to useable data
+        """
+        datah = str(data)
+        datah = datah.replace("]","")
+        datah = datah.replace("[","")
+        datah = datah.replace("}","")
+        datah = datah.replace("{","")
+        datah = datah.replace(":","")
+        datah = datah.replace("'","")
+        datah = datah.replace(",","")
+        datah = list(datah.split(" "))
+        cloud = float(datah[-7])# Say how much cloud there is, it gives a value from 0 to 100. Which is a value showing procentage.
+        amountOfRain = float(datah[-5])# Says amount of precipitation in mm/hr, it can look like 2,45 and 0,15. It is a value above 0
+        weather = int(datah[-3])
+        """ Gives a value based on what type of precipitation, 
+            0 = no precipitation
+            1 = rain
+            2 = snow
+            3 = freezing rain
+            4 = ice pellet
+        """
+        temprature = float(datah[-1])#Gives a value in celsius ít can look like -5 and 12,5
+        print(datah)
 
-if temprature >= 10:
-    print("warm")
+        if temprature >= 10:
+            value = 5
+        return value
+data5 = weather_led_strip.checkToApi()
+print(data5)
